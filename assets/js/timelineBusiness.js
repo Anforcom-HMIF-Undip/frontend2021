14e3d5cbb96ea09db72e7e3138364c3a71faa32d
alvintriseptia <alvintryseptia@gmail.com>
2021-06-28 15:13:51 +0700

    update icon hamburger menu

--- assets/js/timelineBusiness.js
+++ assets/js/timelineBusiness.js
@@ -1,58 +1,64 @@
-const timeline = {
-    first : {day : 20,
-            monthYear : "Mei 2021",
-            description : "Pembukaan Pendaftaran"},
-    second : {day : 10,
-        monthYear : "Agustus 2021",
-        description : "Pembukaan Pendaftaran"},
-    third : {day : 20,
-        monthYear : "Agustus 2021",
-        description : "Pembukaan Pendaftaran"},
-    fourth : {day : 31,
-        monthYear : "Agustus 2021",
-        description : "Pembukaan Pendaftaran"},
-    fifth : {day : 1,
-        monthYear : "September 2021",
-        description : "Pembukaan Pendaftaran"},
-    sixth : {day : 17,
-        monthYear : "September 2021",
-        description : "Pembukaan Pendaftaran"},
-}
+const timeline = [
+    {day : "20",
+    monthYear : "Mei 2021",
+    description : "Pembukaan Pendaftaran"},
+    {day : "10",
+    monthYear : "Agustus 2021",
+    description : "Pembukaan Pendaftaran"},
+    {day : "20",
+    monthYear : "Agustus 2021",
+    description : "Pembukaan Pendaftaran"},
+    {day : "31",
+     monthYear : "Agustus 2021",
+    description : "Pembukaan Pendaftaran"},
+    {day : "1",
+    monthYear : "September 2021",
+    description : "Pembukaan Pendaftaran"},
+    {day : "17",
+    monthYear : "September 2021",
+    description : "Pembukaan Pendaftaran"},
+]
+
 let cardTimeline = document.querySelector(".timelineBusiness");
 cardTimeline.className += " d-flex justify-content-center justify-content-xl-start align-items-center flex-column flex-lg-row gap-3 flex-wrap"
+showTimeline(timeline);
 
-let html = ``;
-let count = 1;
-for (const property in timeline) {
-    if (property == "first"){
-        html += 
-        `<div 
-            data-aos="fade-left"
-            data-aos-once="true"
-            data-aos-delay=${100*count} 
-            class="card-timeline active px-3 px-lg-4 py-3">
-            <div class="date mb-4">
-                <h3 class="day m-0">${timeline[property].day}</h3>
-                <h6 class="monthYear">${timeline[property].monthYear}</h6>
-            </div>
-            <p class="description">${timeline[property].description}</p>
-        </div>`;    
+
+
+
+function showTimeline(timeline){
+    let html = ``;
+    let count = 1;
+    for (let i = 0; i<timeline.length;i++) {
+        if (timeline[i].monthYear == "Mei 2021"){
+            html += 
+            `<div 
+                data-aos="fade-left"
+                data-aos-once="true"
+                data-aos-delay=${100*count} 
+                class="card-timeline active px-3 px-lg-4 py-3">
+                <div class="date mb-4">
+                    <h3 class="day m-0">${timeline[i].day}</h3>
+                    <h6 class="monthYear">${timeline[i].monthYear}</h6>
+                </div>
+                <p class="description">${timeline[i].description}</p>
+            </div>`;    
+        }
+        else{
+            html += 
+            `<div
+                data-aos="fade-left"
+                data-aos-once="true"
+                data-aos-delay=${100*count} 
+                class="card-timeline px-3 px-lg-4 py-3">
+                <div class="date mb-4">
+                    <h3 class="day m-0">${timeline[i].day}</h3>
+                    <h6 class="monthYear">${timeline[i].monthYear}</h6>
+                </div>
+                <p class="description">${timeline[i].description}</p>
+            </div>`;
+        }
+        count++;
     }
-    else{
-        html += 
-        `<div
-            data-aos="fade-left"
-            data-aos-once="true"
-            data-aos-delay=${100*count} 
-            class="card-timeline px-3 px-lg-4 py-3">
-            <div class="date mb-4">
-                <h3 class="day m-0">${timeline[property].day}</h3>
-                <h6 class="monthYear">${timeline[property].monthYear}</h6>
-            </div>
-            <p class="description">${timeline[property].description}</p>
-        </div>`;
-    }
-    count++;
+    cardTimeline.innerHTML += html;
 }
-
-cardTimeline.innerHTML += html;
