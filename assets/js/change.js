13b193b609e9e0bd70967529ca81cfb6f4b3963a
Muhammad Keenan <mkf450@gmail.com>
2021-07-05 14:49:42 +0700

    update changePass + form viladation

--- assets/js/change.js
+++ assets/js/change.js
@@ -1,29 +1,29 @@
 function validateForm() {
     //collect form data in JavaScript variables
-    var name = document.getElementById("fname").value;
-    var email = document.getElementById("femail").value;
     var pw1 = document.getElementById("pswd1").value;
     var pw2 = document.getElementById("pswd2").value;
-    //var check = document.getElementById("flexCheckChecked").checked;
+    var pw3 = document.getElementById("pswd3").value;
 
-    //check empty name field
-    if(name == "") {
-      document.getElementById("nameMsg").innerHTML = "Fill the full name";
+    //check empty confirm password field
+    if(pw3 == "") {
+      document.getElementById("pw3Msg").innerHTML = "Enter the password, please!";
       return false;
     }
 
-    //character data validation
-    if(!isNaN(name)){
-      document.getElementById("nameMsg").innerHTML = "Only characters are allowed";
+    //minimum and maximum password length validation
+    if(pw3.length < 8) {
+      document.getElementById("pw3Msg").innerHTML = "Password length must be at least 8 characters";
       return false;
     }else{
-      document.getElementById("nameMsg").innerHTML = "";
+      document.getElementById("pw3Msg").innerHTML = "";
     }
 
-    //check empty email field
-    if(email ==""){
-      document.getElementById("emailMsg").innerHTML = "Fill the email";
+    //password confirmation
+    if(pw1 == pw3) {
+      document.getElementById("pw1Msg").innerHTML = "Passwords can not be same!";
       return false;
+    }else{
+      document.getElementById("pw1Msg").innerHTML = "";
     }
 
     //check empty password field
@@ -54,12 +54,4 @@ function validateForm() {
       document.getElementById("pw2Msg").innerHTML = "";
     }
 
-    //terms and condition check
-    /*
-    if(check == false){
-      return false;
-    }else{
-      return true;
-    }
-    */
 }
