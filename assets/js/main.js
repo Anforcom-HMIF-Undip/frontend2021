7428fb3b5a6395c32f6f04c2f702d7be0907ba6c
sabdadev <jasonalhilal@gmail.com>
2021-07-14 16:49:23 +0700

    samain navbar, scroll bar, FAQ

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,7 +201,38 @@
       }
     });
   }
+  /*---------------------
+  	 wow .js
+  	--------------------- */
+  function wowAnimation() {
+    new WOW({
+      offset: 100,
+      mobile: true
+    }).init()
+  }
+  wowAnimation()
 
+
+
+
+  /*--------------------------
+   collapse
+  ---------------------------- */
+
+  var panel_test = $('.panel-heading a');
+  panel_test.on('click', function () {
+    panel_test.removeClass('active');
+    $(this).addClass('active');
+  });
+  /*--------------------------
+   Parallax
+  ---------------------------- */
+  var parallaxeffect = $(window);
+  parallaxeffect.stellar({
+    responsive: true,
+    positionProperty: 'position',
+    horizontalScrolling: false
+  });
 
 
 
